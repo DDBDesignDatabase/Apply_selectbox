a51c642a48be0c96f7e773d7ca1139e51d777a3b
hyeewooon <65813977+hyeewooon@users.noreply.github.com>
2020-12-06 22:33:59 +0900

    Add files via upload

---
+++ src/lecture/Learning_Program.java
@@ -0,0 +1,67 @@
+package lecture;
+
+public class Learning_Program {
+	private String NAME;
+	private String nSI_DO;
+	private String nSI_GUN_GU;
+	private String ADDRESS;
+	private String T_NAME;
+	private String OPERATING_DAY;
+	private String HP;
+	private String ON_OFF;
+	
+	public String getNAME() {
+		return NAME;
+	}
+	public void setNAME(String nAME) {
+		NAME = nAME;
+	}
+
+	public String getnSI_DO() {
+		return nSI_DO;
+	}
+	public void setnSI_DO(String nSI_DO) {
+		this.nSI_DO = nSI_DO;
+	}
+
+	public String getnSI_GUN_GU() {
+		return nSI_GUN_GU;
+	}
+	public void setnSI_GUN_GU(String nSI_GUN_GU) {
+		this.nSI_GUN_GU = nSI_GUN_GU;
+	}
+
+	public String getADDRESS() {
+		return ADDRESS;
+	}
+	public void setADDRESS(String ADDRESS) {
+		this.ADDRESS = ADDRESS;
+	}
+
+	public String getT_NAME() {
+		return T_NAME;
+	}
+	public void setT_NAME(String T_NAME) {
+		this.T_NAME = T_NAME;
+	}
+	public String getOPERATING_DAY() {
+		return OPERATING_DAY;
+	}
+	
+	public void setOPERATING_DAY(String oPERATING_DAY) {
+		this.OPERATING_DAY = oPERATING_DAY;
+	}
+
+	public String getHP() {
+		return HP;
+	}
+	public void setHP(String HP) {
+		this.HP = HP;
+	}
+
+	public String getON_OFF() {
+		return ON_OFF;
+	}
+	public void setON_OFF(String ON_OFF) { this.ON_OFF = ON_OFF;
+	}
+}
